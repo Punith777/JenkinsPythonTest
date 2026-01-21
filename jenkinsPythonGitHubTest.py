489734f03ce2c3927d87d96e26fc626a65d18ebf
Punith777 <90909440+Punith777@users.noreply.github.com>
2026-01-21 18:32:01 +0530

    made error correction

--- jenkinsPythonGitHubTest.py
+++ jenkinsPythonGitHubTest.py
@@ -1,4 +1,4 @@
 print("Hello, its working in Jenkins")
 print("Added second line")
 print("Added third line and waiting for jenkins SCM feature to detect the commit and run build automatically")
-print("This comes in DevJob pipeline 1 job"
+print("This comes in DevJob pipeline 1 job")
